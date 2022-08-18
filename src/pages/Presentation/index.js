f9bd8eedd927cd59129159f5631f187fafaaa220
Alejandra Barbosa <alejandrabarbosa1503@gmail.com>
2022-08-18 12:14:40 -0500

    Cambio de Imagen Index

--- src/pages/Presentation/index.js
+++ src/pages/Presentation/index.js
@@ -44,7 +44,7 @@ import routes from "routes";
 import footerRoutes from "footer.routes";
 
 // Images
-import bgImage from "../../assets/images/img_barbosa.jpg";
+import bgImage from "../../assets/images/bg-presentation.jpg";
 
 function Presentation() {
   return (
